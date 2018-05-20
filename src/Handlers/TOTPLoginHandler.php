60087428b17308571db4cd9fde8e2807994810e9
Simon Erkelens <simonerkelens@silverstripe.com>
2018-05-20 12:15:47 +1200

    Update with the bootstrap MFA Updates

--- src/Handlers/TOTPLoginHandler.php
+++ src/Handlers/TOTPLoginHandler.php
@@ -3,40 +3,73 @@
 
 namespace SilverstripeElliot\TOTPAuthenticator;
 
-use Firesphere\BootstrapMFA\MFALoginHandler;
+use Firesphere\BootstrapMFA\Handlers\BootstrapMFALoginHandler;
+use Psr\Container\NotFoundExceptionInterface;
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Control\HTTPResponse;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\ORM\ValidationException;
+use SilverStripe\ORM\ValidationResult;
 use SilverStripe\Security\Member;
+use SilverStripe\Security\PasswordEncryptor_NotFoundException;
 use SilverStripe\Security\Security;
-class TOTPLoginHandler extends MFALoginHandler
+
+/**
+ * Class TOTPLoginHandler
+ * @package SilverstripeElliot\TOTPAuthenticator
+ */
+class TOTPLoginHandler extends BootstrapMFALoginHandler
 {
+    /**
+     * @var array
+     */
     private static $allowed_actions = [
         'MFAForm',
         'validateTOTP'
     ];
 
-    public function validateTOTP($data, $form, $request) {
+    /**
+     * @param $data
+     * @param $form
+     * @param HTTPRequest $request
+     * @return HTTPResponse
+     * @throws NotFoundExceptionInterface
+     * @throws ValidationException
+     * @throws PasswordEncryptor_NotFoundException
+     */
+    public function validateTOTP($data, $form, $request)
+    {
+        $result = Injector::inst()->get(ValidationResult::class);
         $session = $request->getSession();
-        $message = false;
 
         $this->request['BackURL'] = !empty($session->get('MFALogin.BackURL')) ? $session->get('MFALogin.BackURL') : '';
-        $member = $this->authenticator->validateTOTP($data, $request, $message);
-        if(!$member) {
-            $member = parent::validate($data, $form, $request);
+        $member = $this->authenticator->validateTOTP($data, $request, $result);
+
+        if (!$member instanceof Member) {
+            $member = parent::validate($data, $form, $request, $result);
         }
 
-        if ($member instanceof Member) {
-
+        if ($member instanceof Member && $result->isValid()) {
+            $member->MFAEnabled = true;
+            $member->write();
             $memberData = $session->get('MFALogin');
 
             $this->performLogin($member, $memberData, $request);
             Security::setCurrentUser($member);
             $session->clear('MFAForm');
+
             return $this->redirectAfterSuccessfulLogin();
         }
+
         return $this->redirect($this->link());
     }
-    public function MFAForm() {
 
-        return TOTPLoginForm::create(
+    /**
+     * @return static|TOTPForm
+     */
+    public function MFAForm()
+    {
+        return TOTPForm::create(
             $this,
             get_class($this->authenticator),
             'MFAForm'
--- src/Providers/TOTPProvider.php
+++ src/Providers/TOTPProvider.php
@@ -3,31 +3,51 @@
 
 namespace SilverstripeElliot\TOTPAuthenticator;
 
+use Firesphere\BootstrapMFA\Providers\BootstrapMFAProvider;
+use Firesphere\BootstrapMFA\Providers\MFAProvider;
+use lfkeitel\phptotp\Base32;
+use lfkeitel\phptotp\Totp;
+use SilverStripe\Core\Injector\Injector;
+use SilverStripe\ORM\ValidationException;
+use SilverStripe\ORM\ValidationResult;
+use SilverStripe\Security\Member;
+use SilverStripe\Security\PasswordEncryptor_NotFoundException;
 
-use Firesphere\BootstrapMFA\BootstrapMFAProvider;
-use lfkeitel\phptotp\{Base32,Totp};
-
-class TOTPProvider extends BootstrapMFAProvider
+/**
+ * Class TOTPProvider
+ * @package SilverstripeElliot\TOTPAuthenticator
+ */
+class TOTPProvider extends BootstrapMFAProvider implements MFAProvider
 {
+    /**
+     * @param string $token
+     * @param null $result
+     * @return bool|Member
+     * @throws ValidationException
+     * @throws PasswordEncryptor_NotFoundException
+     * @throws \Exception
+     */
     public function verifyToken($token, &$result = null)
     {
+        if (!$result) {
+            $result = Injector::inst()->get(ValidationResult::class);
+        }
         $member = $this->getMember();
-        if($member && $member->ID) {
-            if(!isset($token)) {
+        if ($member && $member->ID) {
+            if (!$token) {
                 $result->addError('Invalid or missing second factor token');
             } else {
                 $secret = Base32::decode($member->TOTPSecret);
                 $key = (new Totp())->GenerateToken($secret);
                 $user_submitted_key = $token;
-                if($user_submitted_key !== $key) {
+                if ($user_submitted_key !== $key) {
                     $result->addError('Invalid or missing second factor token');
                 } else {
                     return $this->member;
                 }
-
-
             }
         }
+
         return parent::verifyToken($token, $result);
     }
 }
